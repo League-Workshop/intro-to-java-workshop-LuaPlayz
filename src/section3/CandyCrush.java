20d9fb5a9a569c3bfcc2dbb2664514f38f25974b
League Student <league@iMac-10.attlocal.net>
2019-10-16 18:28:35 -0700

    CANDY CRUSH

--- src/section3/CandyCrush.java
+++ src/section3/CandyCrush.java
@@ -6,13 +6,13 @@ public class CandyCrush {
 
 	public static void main(String[] args) {
 		// 1. Using a pop-up, ask the user who they don't like
-
+String str2=JOptionPane.showInputDialog("WHO DO U NOOOOOT LIKE?!");
 		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
-		JOptionPane.showMessageDialog(null, "You totally have a crush on Java!");
+		JOptionPane.showMessageDialog(null, "You totally have a crush on "+str2+"!");
 		// 3. Ask the user for the name of their best friend
-
+String str3=JOptionPane.showInputDialog("WHO IS UR BFF?!");
 		// 4. Tell them their best friend is as sweet as candy
-
+JOptionPane.showMessageDialog(null, str3+ " IS SOOOO SWEET!!!!");
 	} 
 }
 
---
+++ src/section3/HelloWorld.java
@@ -0,0 +1,17 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+public static void main(String[] args) {
+	System.out.println("I CAST A SPELL ON U FROM THE DEPTHS OF TATARUS");
+	JOptionPane.showMessageDialog(null, "*runs to CVS*");
+	String str = "WHERE R DA SLURPEESS?! I NED TO ATTACK 4 BOYS FOR SELF-DEFENSE!";
+	System.out.println(str);
+	String str2= JOptionPane.showInputDialog("str");
+	System.out.println(str2);
+	JOptionPane.showMessageDialog(null, "THEY R AFTER MEHHH!!!");
+	System.out.println("COME BACK HERE"+str2+".");
+	System.out.println("I SAID COME BACK HERE"+str2+".");
+}}
+
